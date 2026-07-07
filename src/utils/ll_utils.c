cff249c0923e97af23ed4331bebc6f8b7d74ab6d
0xurandom <225617807+0xurandom@users.noreply.github.com>
2026-07-07 18:58:08 +0530

    add linked list utils

--- src/utils/ll_utils.c
+++ src/utils/ll_utils.c
@@ -12,6 +12,18 @@ LL_Node initLL_Node(char *key, size_t key_length, double value) {
     return node;
 }
 
-void setNextLL_Node(LL_Node *node, LL_Node *next) {
-    
+void setLL_Node(LL_Node *node, double value) { node->value = value; }
+
+void setNextLL_Node(LL_Node *node, LL_Node *next_node) {
+    node->next = next_node;
+    next_node->next = NULL;
+}
+
+void freeLL_Node(LL_Node *node) {
+    freeStringView(&node->key);
+    node->value = 0;
+
+    if (node->next != NULL) {
+        freeLL_Node(node->next);
+    }
 }
--- src/utils/ll_utils.h
+++ src/utils/ll_utils.h
@@ -10,3 +10,8 @@ struct LL_Node {
 
     LL_Node *next;
 };
+
+LL_Node initLL_Node(char *key, size_t key_length, double value);
+void setLL_Node(LL_Node *node, double value);
+void setNextLL_Node(LL_Node *node, LL_Node *next_node);
+void freeLL_Node(LL_Node *node);
