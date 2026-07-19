910eb5795d5eccc129c06cf3836c52609a3b8c0f
0xurandom <225617807+0xurandom@users.noreply.github.com>
2026-07-19 23:13:50 +0530

    remove redundant complex funcs

--- src/utils/complex_utils.c
+++ src/utils/complex_utils.c
@@ -1,75 +1,27 @@
 #include "complex_utils.h"
 
-#include <math.h>
-#include <stdlib.h>
+void getSqrtofComplex(double complex num, double complex* result1,
+                      double complex* result2) {
+    double magnitude = cabs(num);
+    double argument = carg(num);
 
-// TODO: use complex.h
-
-void getComplexSqrt(double num, ComplexNum *result) {
-    result->real = 0;
-    result->imag = sqrt(num);
+    *result1 = csqrt(num);
+    *result2 = -(*result1);
 }
 
 double complex ccbrt(double complex num) { return cexp(clog(num) / 3); }
 
-void getCbrtofComplex(ComplexNum *num, ComplexNum *result1, ComplexNum *result2,
-                      ComplexNum *result3) {
-    double magnitude = getMagnitude(num);
-    double argument = getArgument(num);
+void getCbrtofComplex(double complex num, double complex* result1,
+                      double complex* result2, double complex* result3) {
+    double magnitude = cabs(num);
+    double argument = carg(num);
 
-    result1->real = (cbrt(magnitude)) * cos((argument / 3));
-    result1->imag = (cbrt(magnitude)) * sin((argument / 3));
+    *result1 = (cbrt(magnitude)) * cos((argument / 3)) +
+               (cbrt(magnitude)) * sin((argument / 3)) * I;
 
-    result2->real = (cbrt(magnitude)) * cos(((argument + (2 * M_PI)) / 3));
-    result2->imag = (cbrt(magnitude)) * sin(((argument + (2 * M_PI)) / 3));
+    *result2 = (cbrt(magnitude)) * cos(((argument + (2 * M_PI)) / 3)) +
+               (cbrt(magnitude)) * sin(((argument + (2 * M_PI)) / 3)) * I;
 
-    result3->real = (cbrt(magnitude)) * cos(((argument + (4 * M_PI)) / 3));
-    result3->imag = (cbrt(magnitude)) * sin(((argument + (4 * M_PI)) / 3));
-}
-
-void addComplexNums(ComplexNum *num1, ComplexNum *num2, ComplexNum *result) {
-    result->real = num1->real + num2->real;
-    result->imag = num1->imag + num2->imag;
-}
-
-void subtractComplexNums(ComplexNum *num1, ComplexNum *num2,
-                         ComplexNum *result) {
-    result->real = num1->real - num2->real;
-    result->imag = num1->imag - num2->imag;
-}
-
-void raiseComplexNum(ComplexNum *num, size_t power, ComplexNum *result) {
-    ComplexNum originalNum = {
-        .real = num->real,
-        .imag = num->imag,
-    };
-
-    for (size_t i = 0; i < power; i++) {
-        multiplyComplexNums(num, &originalNum, num);
-    }
-}
-
-void divideComplexNums(ComplexNum *num1, ComplexNum *num2, ComplexNum *result) {
-    result->real = ((num1->real * num2->real) + (num1->imag * num2->imag)) /
-                   (pow(num2->real, 2) + pow(num2->imag, 2));
-    result->imag = ((num1->imag * num2->real) + (num1->real * num2->imag)) /
-                   (pow(num2->real, 2) + pow(num2->imag, 2));
-}
-
-void conjugateComplexNum(ComplexNum *num) { num->imag = -num->imag; }
-
-double getArgument(ComplexNum *num) { return atan(num->imag / num->real); }
-
-double getMagnitude(ComplexNum *num) {
-    return (pow(num->real, 2) + pow(num->imag, 2));
-}
-
-void rotateComplexNum(ComplexNum *num, double rad) {
-    ComplexNum temp = {
-        .real = num->real,
-        .imag = num->imag,
-    };
-
-    num->real = (temp.real * cos(rad)) - (temp.imag * sin(rad));
-    num->imag = (temp.real * sin(rad)) + (temp.imag * cos(rad));
+    *result3 = (cbrt(magnitude)) * cos(((argument + (4 * M_PI)) / 3)) +
+               (cbrt(magnitude)) * sin(((argument + (4 * M_PI)) / 3)) * I;
 }
--- src/utils/complex_utils.h
+++ src/utils/complex_utils.h
@@ -4,21 +4,10 @@
 #include <math.h>
 #include <stddef.h>
 
-typedef struct {
-    double real;
-    double imag;
-} ComplexNum;
+void getSqrtofComplex(double complex num, double complex* result1,
+                      double complex* result2);
 
 double complex ccbrt(double complex num);
-void initComplexNum(ComplexNum *num);
-void addComplexNums(ComplexNum *num1, ComplexNum *num2, ComplexNum *result);
-void subtractComplexNums(ComplexNum *num1, ComplexNum *num2,
-                         ComplexNum *result);
-void multiplyComplexNums(ComplexNum *num1, ComplexNum *num2,
-                         ComplexNum *result);
-void raiseComplexNum(ComplexNum *num, size_t power, ComplexNum *result);
-void divideComplexNums(ComplexNum *num1, ComplexNum *num2, ComplexNum *result);
-void conjugateComplexNum(ComplexNum *num);
-double getArgument(ComplexNum *num);
-double getMagnitude(ComplexNum *num);
-void rotateComplexNum(ComplexNum *num, double rad);
+
+void getCbrtofComplex(double complex num, double complex* result1,
+                      double complex* result2, double complex* result3);
